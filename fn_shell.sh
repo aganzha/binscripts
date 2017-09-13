ca6ad352cc14e78ae3e8e379cd790a6817a9c41d
Alexey Ganzha <aganzha@yandex.ru>
2017-09-13 07:55:09 +0000

    fix pg script

---
+++ fn_shell.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker exec -it fndocker_web_1 python manage.py shell
--- update_dev_fn_fb.sh
+++ update_dev_fn_fb.sh
@@ -13,6 +13,6 @@ sftp aganzha@10.88.114.93:/home/aganzha/arch/current.sql.tar.gz
 gzip -d current.sql.tar.gz 
 sudo mv current.sql.tar postgres_data/current.sql.tar
 echo 'drop database if exists app;create database app;grant all privileges on database app to app;' | docker exec -i postgres psql -U postgres
-docker exec -it postgres pg_restore -v -d app -U postgres /var/lib/postgresql/data/current.sql.tar
+docker exec -i postgres pg_restore -v -d app -U postgres /var/lib/postgresql/data/current.sql.tar
 sudo rm postgres_data/current.sql.tar
 
