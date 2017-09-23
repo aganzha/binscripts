452bc36b2a11bc9239e7806622416275c0a26ce7
Alexey Ganzha <aganzha@yandex.ru>
2017-09-23 08:58:17 +0000

    different webpacks

--- update_dev_fn_fb.sh
+++ update_dev_fn_fb.sh
@@ -9,7 +9,7 @@ else
     echo 'found postgres'
 fi
 cd /mnt/100ext4/
-sftp aganzha@10.88.114.93:/home/aganzha/arch/current.sql.tar.gz
+sftp aganzha@foundersnetwork.com:/home/aganzha/arch/current.sql.tar.gz
 gzip -d current.sql.tar.gz 
 sudo mv current.sql.tar postgres_data/current.sql.tar
 echo 'drop database if exists app;create database app;grant all privileges on database app to app;' | docker exec -i postgres psql -U postgres
